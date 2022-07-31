6628fd5bc34766376d798a1ebd5f40c8603dec48
सबिन (Sabin) <sabin.bajracharya@gmail.com>
2022-07-31 23:48:24 +0200

    Add random navigation and graph plot

---
+++ gridcells/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using gridcells;
+
+var spatial = new SpatialNavigation();
+
+
+var conv = spatial.Conv(45);
+
+
+Console.WriteLine(conv.Item1);
+Console.WriteLine(conv.Item2);
+
+spatial.RandomNavigation(100);
+spatial.Plot();
